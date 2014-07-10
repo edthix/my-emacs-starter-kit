77d4d713ddd59e2e310e62e544b31bfd5396d2e1
Edham Arief Dawillah <edthix@gmail.com>
2014-07-11 06:55:26 +0800

    2014 cleanup

--- init.el
+++ init.el
@@ -1,25 +1,25 @@
-
 ;; Switch caps-lock and right ctrl
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
 
 ;; indent the buffer
 ;; http://stackoverflow.com/questions/4090793/emacs-reindenting-entire-c-buffer
-(defun indent-buffer ()
-  "Indents an entire buffer using the default intenting scheme."
-  interactive)
-(save-excursion
-  (delete-trailing-whitespace)
-  (indent-region (point-min) (point-max) nil)
-  (untabify (point-min) (point-max)))
-(global-set-key (kbd "C-c TAB") 'indent-buffer)
+;; (defun indent-buffer ()
+;;   "Indents an entire buffer using the default intenting scheme."
+;;   interactive)
+;; (save-excursion
+;;   (delete-trailing-whitespace)
+;;   (indent-region (point-min) (point-max) nil)
+;;   (untabify (point-min) (point-max)))
+;; (global-set-key (kbd "C-c TAB") 'indent-buffer)
 
 ;; quit stuffs
 (global-set-key (kbd "C-M-g") 'keyboard-quit)
 (global-set-key (kbd "C-x C-g") 'keyboard-quit)
 
 ;; set ~/public_html as default director
-(setq default-directory "C:/xampp/htdocs")
+(setq default-directory "C:/xampp/htdocs") ;; Windows
+;; (setq default-directory "~public_html") ;; Linux
 
 ;; Put some kungfu for emacs
 ;;(recentf-mode t)
@@ -55,52 +55,37 @@
 (package-initialize)
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit 
-                      ;; Ruby
-                      starter-kit-ruby inf-ruby rinari rspec-mode
-                      ruby-compilation ruby-end
-                      ruby-mode ruby-test-mode flymake-ruby
-                      
-                      ;; PHP
-                      php+-mode flymake-php
+;; (defvar my-packages
+;;   '(starter-kit 
+;;     ;; Ruby
+;;     starter-kit-ruby inf-ruby rinari rspec-mode
+;;     ruby-compilation ruby-end
+;;     ruby-mode ruby-test-mode flymake-ruby
+    
+;;     ;; PHP
+;;     php+-mode flymake-php
 
-                      ;; CSS
-                      flymake-css
+;;     ;; CSS
+;;     flymake-css
 
-                      ;; YAML
-                      yaml-mode
+;;     ;; YAML
+;;     yaml-mode
 
-                      ;; JS
-                      starter-kit-js flymake-jslint
+;;     ;; JS
+;;     starter-kit-js flymake-jslint
 
-                      ;; Clojure
-                      ac-nrepl align-cljlet cljsbuild-mode clojure-mode
-                      clojure-test-mode nrepl
-                      slamhound starter-kit-lisp
-                      ;; clojurescript-mode
-                      )
-  
-  "A list of packages to ensure are installed at launch.")
+;;     ;; Clojure
+;;     ac-nrepl align-cljlet cljsbuild-mode clojure-mode
+;;     clojure-test-mode nrepl
+;;     slamhound starter-kit-lisp
+;;     ;; clojurescript-mode
+;;     )  
+;;   "A list of packages to ensure are installed at launch.")
 
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
+;; (dolist (p my-packages)
+;;   (when (not (package-installed-p p))
+;;     (package-install p)))
 
 ;; TODO
 ;; Fix issue https://github.com/technomancy/emacs-starter-kit/pull/145
-(defalias 'inf-ruby-keys 'inf-ruby-setup-keybindings)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
- '(custom-enabled-themes (quote (cyberpunk)))
- '(custom-safe-themes (quote ("ea0c5df0f067d2e3c0f048c1f8795af7b873f5014837feb0a7c8317f34417b04" "6bc195f4f8f9cf1a4b1946a12e33de2b156ce44e384fbc54f1bae5b81e3f5496" "f38dd27d6462c0dac285aa95ae28aeb7df7e545f8930688c18960aeaf4e807ed" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; (defalias 'inf-ruby-keys 'inf-ruby-setup-keybindings)
