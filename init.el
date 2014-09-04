7a3a8fa259d5f29eb6f52c47ecaf79e4524e99bf
Edham Arief / edthix <edthix@gmail.com>
2014-09-04 23:28:58 +0800

    Support inf-ruby

--- init.el
+++ init.el
@@ -97,27 +97,35 @@
 ;; TODO
 ;; Fix issue https://github.com/technomancy/emacs-starter-kit/pull/145
 ;; (defalias 'inf-ruby-keys 'inf-ruby-setup-keybindings)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-term-color-vector [unspecified "#1F1611" "#660000" "#144212" "#EFC232" "#5798AE" "#BE73FD" "#93C1BC" "#E6E1DC"])
- '(custom-enabled-themes (quote (misterioso)))
- '(custom-safe-themes (quote ("c4e6fe8f5728a5d5fd0e92538f68c3b4e8b218bcfb5e07d8afff8731cc5f3df0" "146d24de1bb61ddfa64062c29b5ff57065552a7c4019bee5d869e938782dfc2a" default)))
- '(fci-rule-character-color "#452E2E")
- '(fci-rule-color "#452E2E")
- '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 ;; Ruby settings
 ;; http://lorefnon.me/2014/02/02/configuring-emacs-for-rails.html
-;; (add-hook 'enh-ruby-mode-hook 'flymake-ruby-load)
-;; (global-set-key (kbd "C-c r r") 'inf-ruby)
+(add-hook 'ruby-mode-hook 'flymake-ruby-load)
+
+;; Ruby repl inside buffer
+(global-set-key (kbd "C-c r r") 'inf-ruby)
+
+;; projectile
+
+
+
+
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(ansi-term-color-vector [unspecified "#1F1611" "#660000" "#144212" "#EFC232" "#5798AE" "#BE73FD" "#93C1BC" "#E6E1DC"])
+;;  '(custom-enabled-themes (quote (misterioso)))
+;;  '(custom-safe-themes (quote ("c4e6fe8f5728a5d5fd0e92538f68c3b4e8b218bcfb5e07d8afff8731cc5f3df0" "146d24de1bb61ddfa64062c29b5ff57065552a7c4019bee5d869e938782dfc2a" default)))
+;;  '(fci-rule-character-color "#452E2E")
+;;  '(fci-rule-color "#452E2E")
+;;  '(vc-annotate-background "#2B2B2B")
+;;  '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
+;;  '(vc-annotate-very-old-color "#DC8CC3"))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  )
