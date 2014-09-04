28e8e2cf9e91dfb08e5ddd4bba8d46ff0738465f
Edham Arief / edthix <edthix@gmail.com>
2014-09-04 23:30:12 +0800

    flx-ido bake in

--- init.el
+++ init.el
@@ -109,7 +109,14 @@
 (projectile-global-mode)
 (add-hook 'ruby-mode-hook 'projectile-on)
 
-
+;; flx-ido decorations
+(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+(defun ido-disable-line-truncation () (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
+(defun ido-define-keys () ;; C-n/p is more intuitive in vertical layout
+  (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
+  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
+(add-hook 'ido-setup-hook 'ido-define-keys)
 
 
 
