4446010132f6bbb8fc330ec94bd934d24f2e9104
Edham Arief / edthix <edthix@gmail.com>
2016-03-26 05:56:06 +0800

    Enable zenburn

--- init.el
+++ init.el
@@ -200,3 +200,18 @@
 ;;******************************************************************************
 ;; Optional for Mac
 ;;******************************************************************************
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("316d29f8cd6ca980bf2e3f1c44d3a64c1a20ac5f825a167f76e5c619b4e92ff4" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
